a0535244b33b1b1f5bb1c7b2eb55ecffdb58dd2a
rqtrinh <rqtrinh@cpp.edu>
2021-09-23 23:54:41 -0700

    Initial commit for Guessing Game program with outline in comments

---
+++ guessingGame.c
@@ -0,0 +1,38 @@
+/*Import necessary libraries*/
+
+
+/*Create main function with input parameters argc and argv
+so that we can accept input from command line*/
+
+    /*Init int programRun to 0*/
+    /*Init int guessRun to 0*/
+    /*Init int  default max to 10*/
+    /*Declare int "userInput", "guessNumber", "userGuess"*/
+    
+
+    /*Init random number genereator*/
+
+    /*Create a do while loop that will run while programRun = 0 */
+
+        /*Print out menu with options*/
+        /*Scan user input and set "userInput" equal to it*/
+        /*Switch statment for 3 options using "userInput"*/
+    
+            /*Option 1*/
+                /*Create random number starting from 1 to max and set it equal to guessNumber*/
+                /*Do while loop while guessRun = 0*/
+                    /*Ask for user's guess*/
+                    /*Scan for the user's input*/
+                    /*If user guesses correct set guessRun = */
+
+            /*Option 2*/
+                /*Tell them the max number they can set*/
+                /*Ask what they want to make the max number*/
+                /*Change max*/
+
+            /*Option 3*/
+                /*End the game and let the user know*/
+                
+
+
+
